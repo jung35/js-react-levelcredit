bf74082ef463c9eb70793d8a99f538991e2aad03
Jung Oh <jung@jungoh.me>
2021-07-09 15:11:59 -0400

    RT-13087: add account status ui component

--- src/CreditAPI/insights/index.ts
+++ src/CreditAPI/insights/index.ts
@@ -1,8 +1,7 @@
-import useInsights from "src/CreditAPI/insights/useInsights";
-import AccountSummary from "src/CreditAPI/insights/AccountSummary/AccountSummary";
-import CreditBalance from "src/CreditAPI/insights/CreditBalance/CreditBalance";
-import CreditUtilization from "src/CreditAPI/insights/CreditUtilization/CreditUtilization";
-import HardInquiry from "src/CreditAPI/insights/HardInquiry/HardInquiry";
-import LoanType from "src/CreditAPI/insights/LoanType/LoanType";
-
-export { useInsights, AccountSummary, CreditBalance, CreditUtilization, HardInquiry, LoanType };
+export { default as useInsights } from "src/CreditAPI/insights/useInsights";
+export { default as AccountSummary } from "src/CreditAPI/insights/AccountSummary/AccountSummary";
+export { default as CreditBalance } from "src/CreditAPI/insights/CreditBalance/CreditBalance";
+export { default as CreditUtilization } from "src/CreditAPI/insights/CreditUtilization/CreditUtilization";
+export { default as HardInquiry } from "src/CreditAPI/insights/HardInquiry/HardInquiry";
+export { default as LoanType } from "src/CreditAPI/insights/LoanType/LoanType";
+export { default as AccountStatus } from "src/CreditAPI/insights/AccountStatus/AccountStatus";
